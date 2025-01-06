66788a708a592696e0f6de05c682370c8817fd8c
Vasanth Kumar S <vasanthkumarmfs@gmail.com>
2025-01-06 11:11:26 +0530

    my portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
     <section id="contact" className="contact">
       <h2>Contact Me</h2>
       <ul>
-      <li><p>Email: vasanthkumarmfs@gmail.com</p></li>
-      <li><p><a href="https://www.linkedin.com/in/vasanth-kumar-s-254131339" target="_blank" rel="noopener noreferrer">LinkedIn</a></p></li>
-      <li><p><a href="https://github.com/Vasatrix" target="_blank" rel="noopener noreferrer">GitHub</a></p></li>
+      <li><p>Email: youremail </p></li>
+      <li><p><a href="LinkedIn_link" target="_blank" rel="noopener noreferrer">LinkedIn</a></p></li>
+      <li><p><a href="GitHub_link" target="_blank" rel="noopener noreferrer">GitHub</a></p></li>
       </ul>
     </section>
   );
